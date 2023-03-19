a3039418f897fdace3cbba45deb21f772d44c3bd
Yerick Rafaldy De Fretes <yerickrafaldy99@gmail.com>
2023-03-19 22:04:00 +0700

    first commit

---
+++ go.go
@@ -0,0 +1,9 @@
+func main() {
+}
+var n,i int
+fmt.Scan(&n)
+//program cetak 1 ... n
+//sintaks for ==> pseudocode for i=1 to nd do (i++)
+for i=1 ; i <n ; i=i+1 { // kondisi masuk looping
+}
+	fmt.print(i)
